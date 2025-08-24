5e2e28c879e90257285be06001e032a6a4f25d7a
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2025-08-24 14:46:57 -0300

    Update api.service.js

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,43 +1,46 @@
 import axios from "axios";
 
+
+const VITE_OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
 export const api = axios.create({
- baseURL: "http://www.omdbapi.com/",
- params: {
-  apikey: "b87b88b9",
+ baseURL: "http://www.omdbapi.com/",
+ params: {
+  apikey: VITE_OMDB_API_KEY, 
  },
 });
 
 export async function searchMovies(title, page = 1) {
-    try {
-        const response = await api.get("/", {
-            params: {
-                s: title,
-                page: page,
-            },
-        });
-        if (response.data.Response === "False") {
-            throw new Error(response.data.Error || "Nenhum filme encontrado.");
-        }
-        return response.data;
-    } catch (error) {
-        console.error("Error searching movies:", error.message);
-        throw error;
-    }
+    try {
+        const response = await api.get("/", {
+            params: {
+                s: title,
+                page: page,
+            },
+        });
+        if (response.data.Response === "False") {
+            throw new Error(response.data.Error || "Nenhum filme encontrado.");
+        }
+        return response.data;
+    } catch (error) {
+        console.error("Error searching movies:", error.message);
+        throw error;
+    }
 }
 
 export async function getMovieDetails(imdbID) {
-    try {
-        const response = await api.get("/", {
-            params: {
-                i: imdbID,
-            },
-        });
-        if (response.data.Response === "False") {
-            throw new Error(response.data.Error || "Não foi possível carregar os detalhes.");
-        }
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching movie details:", error.message);
-        throw error;
-    }
+    try {
+        const response = await api.get("/", {
+            params: {
+                i: imdbID,
+            },
+        });
+        if (response.data.Response === "False") {
+            throw new Error(response.data.Error || "Não foi possível carregar os detalhes.");
+        }
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching movie details:", error.message);
+        throw error;
+    }
 }
